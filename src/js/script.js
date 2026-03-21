60f5b36cc1a8dd228ff16771742c402206ea3fd3
denisguedrian <denisguedrian@gmail.com>
2026-03-20 21:47:24 -0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,47 @@ $(document).ready(function() {
         $('.navbar__menu-mobile').toggleClass('active');
         $('.navbar__btn-mobile').find('i').toggleClass('fa-bars fa-x');
     });
+
+    const sections = $('section');
+    const navItens = $('.navbar__item');
+
+
+    // navItens.on('click', function() {
+    //     navItens.removeClass('active');
+    //     $(this).addClass('active');
+    // });
+
+
+    $(window).on('scroll', function (){
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        }else{
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+
+        sections.each(function (i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 91;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+        navItens.removeClass('active');
+        $(navItens[activeSectionIndex]).addClass('active');
+
+    });
+
+    ScrollReveal().reveal('.hero__content', { origin: 'left', distance: '60px', duration: 2000 });
+    ScrollReveal().reveal('.dish', { origin: 'bottom', distance: '40px', duration: 600, interval: 150 });
+    ScrollReveal().reveal('.feedback', { origin: 'bottom', distance: '30px', duration: 600, interval: 200 });
+
 });
